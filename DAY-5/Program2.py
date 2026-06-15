ba5075472122240276e0b19599c7209c452bf172
shivanikundu <shivani20051114@gmail.com>
2026-06-15 17:19:47 +0530

    add files via upload

--- DAY-5/Program2.py
+++ DAY-5/Program2.py
@@ -1,6 +1,6 @@
 #Count word frequency in a sentence using a dictionary
 sentence = input("Enter a sentance : ")
-words = sentence.lower().split()   #.lower() is used to converts al characters in a string to lowercase and .split() is used to break that string into a list
+words = sentence.split()   #.split() is used to break that string into a list
 frequency = {}
 for word in words :
   if word in frequency :
