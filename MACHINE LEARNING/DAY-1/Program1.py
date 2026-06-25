0c363326fab2c07c0c603c637c3ba6e028edb403
shivanikundu <shivani20051114@gmail.com>
2026-06-25 16:47:40 +0530

    Add files via upload

---
+++ MACHINE LEARNING/DAY-1/Program1.py
@@ -0,0 +1,11 @@
+
+import sklearn
+import pandas as pd
+import numpy as np
+import seaborn as sns
+
+print(f"Scikit-learn version: {sklearn.__version__}")
+print(f"Pandas version: {pd.__version__}")
+print(f"NumPy version: {np.__version__}")
+print(f"seaborn version: {sns.__version__}")
+print("Environment setup successful!")
